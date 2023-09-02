eb7f9ecb23b0d598bb3119459587e7a8ff83e1cc
Saoa35 <saoamah@gmail.com>
2023-09-02 17:33:59 +0400

    + created NavBar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,10 +1,50 @@
-import React from "react";
+import Link from "next/link";
+
+const links = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Portfolio",
+    url: "/portfolio",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 4,
+    title: "About",
+    url: "/about",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    url: "/contact",
+  },
+  {
+    id: 6,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+];
 
 function NavBar() {
   return (
-    <div>
-      <div>NavBar</div>
-    </div>
+    <nav>
+      <Link href="/">MyIntroduction</Link>
+      <div>
+        {links.map((link) => (
+          <Link key={link.id} href={link.url}>
+            {link.url}
+          </Link>
+        ))}
+      </div>
+    </nav>
   );
 }
 
