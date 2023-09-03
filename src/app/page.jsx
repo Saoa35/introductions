703e2a576a771c17abde3ec73bdbc8fbda04b802
Saoa35 <saoamah@gmail.com>
2023-09-03 11:43:18 +0400

    + created Button component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,5 +2,15 @@ import React from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <div className={styles.container}>Home</div>;
+  return (
+    <main className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>The Future of AI in the next few years</h1>
+        <p className={styles.desc}>
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.
+        </p>
+      </div>
+    </main>
+  );
 }
