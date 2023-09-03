25b2eb0635d081d5ecc18afd97a73328e38602c0
Saoa35 <saoamah@gmail.com>
2023-09-03 11:17:41 +0400

    + created Footer comp.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,41 @@
 import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
 
 function Footer() {
   return (
-    <footer>
+    <footer className={styles.container}>
       <div>2023 development</div>
-      <div></div>
+      <div className={styles.social}>
+        <Image
+          src="/telegramm.png"
+          width={20}
+          height={20}
+          className={styles.icon}
+          alt="telegramm"
+        />
+        <Image
+          src="/skype.jpg"
+          width={20}
+          height={20}
+          className={styles.icon}
+          alt="skype"
+        />
+        <Image
+          src="/linkedIn.png"
+          width={20}
+          height={20}
+          className={styles.icon}
+          alt="linkedIn"
+        />
+        <Image
+          src="/github.png"
+          width={20}
+          height={20}
+          className={styles.icon}
+          alt="github"
+        />
+      </div>
     </footer>
   );
 }
