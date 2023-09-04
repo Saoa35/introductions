b115d4837fca12ec838a31caa882311fc6e41233
Saoa35 <saoamah@gmail.com>
2023-09-04 13:44:10 +0400

    + created blog id file

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
 
-function BlogId() {
+function BlogId({ params }) {
   return (
     <div>
-      <div>BlogId</div>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>Description</p>
+          <div className={styles.author}>
+            <Image
+              src="/#"
+              alt="pict"
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span>User name</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src="/#" alt="pict" fill={true} className={styles.image} />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p>Content</p>
+      </div>
     </div>
   );
 }
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,44 @@
 import React from "react";
 import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 function Blog() {
-  return <div className={styles.container}>Blog</div>;
+  return (
+    <div>
+      <Link href="blog/test" className={styles.container} key="1">
+        <div>
+          <Image
+            src="https://img.freepik.com/free-vector/house-with-save-world-icons_24877-50575.jpg"
+            alt="blog"
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>Description</p>
+        </div>
+      </Link>
+
+      <Link href="blog/test2" className={styles.container} key="2">
+        <div>
+          <Image
+            src="https://img.freepik.com/free-vector/house-with-save-world-icons_24877-50575.jpg"
+            alt="blog"
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>Description</p>
+        </div>
+      </Link>
+    </div>
+  );
 }
 
 export default Blog;
