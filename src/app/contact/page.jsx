356d4dab1ac433b76a26a23d5e6a36dbb86f672c
Saoa35 <saoamah@gmail.com>
2023-09-04 10:14:25 +0400

    * changed global styles

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/button/Button";
 
 function Contact() {
   return (
     <div>
-      <div>Contact</div>
+      <h1 className={styles.title}>Let`s Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/contact.jpg"
+            fill={true}
+            className={styles.image}
+            alt="contacts"
+          />
+        </div>
+
+        <form className={styles.form}>
+          <input type="text" placeholder="Name" className={styles.input} />
+          <input type="email" placeholder="Email" className={styles.input} />
+          <textarea
+            cols="30"
+            rows="10"
+            placeholder="Message"
+            className={styles.textArea}
+          ></textarea>
+          <Button url="#" text="Send" />
+        </form>
+      </div>
     </div>
   );
 }
