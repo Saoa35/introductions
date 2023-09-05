51c1b293c80a9c382bd52fdbecc870e79a8a8c2c
Saoa35 <saoamah@gmail.com>
2023-09-05 12:21:39 +0400

    * fixed bug in route.js

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,13 +1,10 @@
+import Post from "@/models/Post";
 import connect from "@/utils/db";
 import { NextResponse } from "next/server";
 
 export const GET = async (request) => {
   try {
     await connect();
-
-    const mongoose = require("mongoose");
-    const Post = mongoose.model("Post");
-
     const posts = await Post.find();
 
     return new NextResponse(JSON.stringify(posts), { status: 200 });
