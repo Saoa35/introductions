e7d826352c860d24fccbfb1d43149dcf87989174
Saoa35 <saoamah@gmail.com>
2023-09-06 14:34:38 +0400

    * fixed bugs

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -6,7 +6,7 @@ import Button from "@/components/button/Button";
 function Contact() {
   return (
     <div>
-      <h1 className={styles.title}>Let`s Keep in Touch</h1>
+      <h1 className={styles.title}>Lets Keep in Touch</h1>
       <div className={styles.content}>
         <div className={styles.imgContainer}>
           <Image
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,18 +2,16 @@
 
 import useSWR from "swr";
 import styles from "./page.module.css";
-import Image from "next/image";
-import React from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
-function Dashboard() {
+const Dashboard = () => {
   const session = useSession();
   const router = useRouter();
 
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
-
-  const { data, error, isLoading } = useSWR(
+  const { data, mutate, error, isLoading } = useSWR(
     `/api/posts?username=${session?.data?.user.name}`,
     fetcher
   );
@@ -21,20 +19,41 @@ function Dashboard() {
   if (session.status == "loading") {
     return <p>Loading...</p>;
   }
-
   if (session.status == "unauthenticated") {
     router?.push("/dashboard/login");
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-
     const title = e.target[0].value;
     const desc = e.target[1].value;
     const img = e.target[2].value;
     const content = e.target[3].value;
 
     try {
+      await fetch("/api/posts", {
+        method: "POST",
+        body: JSON.stringify({
+          title,
+          desc,
+          img,
+          content,
+          username: session.data.user.name,
+        }),
+      });
+      mutate();
+      e.target.reset();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await fetch(`/api/posts/${id}`, {
+        method: "DELETE",
+      });
+      mutate();
     } catch (error) {
       console.log(error);
     }
@@ -45,8 +64,8 @@ function Dashboard() {
       <div className={styles.container}>
         <div className={styles.posts}>
           {isLoading
-            ? "Loading..."
-            : data.map((post) => (
+            ? "Loading"
+            : data?.map((post) => (
                 <div className={styles.post} key={post._id}>
                   <div className={styles.imgContainer}>
                     <Image
@@ -56,29 +75,28 @@ function Dashboard() {
                       height={100}
                     />
                   </div>
-                  <h2>{post.title}</h2>
-                  <span className={styles.delete}>X</span>
+                  <h2 className={styles.postTitle}>{post.title}</h2>
+                  <span
+                    className={styles.delete}
+                    onClick={() => handleDelete(post._id)}
+                  >
+                    X
+                  </span>
                 </div>
               ))}
         </div>
 
         <form className={styles.new} onSubmit={handleSubmit}>
           <h1>Add New Post</h1>
-
           <input type="text" placeholder="Title" className={styles.input} />
-          <input
-            type="text"
-            placeholder="Description"
-            className={styles.input}
-          />
+          <input type="text" placeholder="Desc" className={styles.input} />
           <input type="text" placeholder="Image" className={styles.input} />
-          <textarea className={styles.textArea} cols={30} rows={10} />
-
+          <textarea cols="30" rows="10" className={styles.textArea} />
           <button className={styles.button}>Send</button>
         </form>
       </div>
     );
   }
-}
+};
 
 export default Dashboard;
